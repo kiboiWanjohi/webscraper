6aa4c9c26f370b0748ec6cc085f5ec7ba400c465
kiboiWanjohi <sammy313.sw@gmail.com>
2023-12-18 00:23:11 +0300

    Webscraper

---
+++ Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import logo from "";
+const Header = () => {
+  const today = new Date().toString().slice(0, 10);
+  return (
+    <div>
+      <header>
+        <div className="textContainer">
+          <h1>{Dealfinder}</h1>
+          <p>{today}</p>
+        </div>
+        <div className="logo-container">
+          <img src={logo} alt="logo" />
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
